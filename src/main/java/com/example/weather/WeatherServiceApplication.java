c84c235fb18ffba9533ec8065d146898ea752ac1
jueun275 <52851721+jueun275@users.noreply.github.com>
2025-04-06 17:47:15 +0900

    transaction 설정 적용

--- src/main/java/com/example/weather/WeatherServiceApplication.java
+++ src/main/java/com/example/weather/WeatherServiceApplication.java
@@ -2,8 +2,10 @@ package com.example.weather;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@EnableTransactionManagement // 트랜잭션 AOP 프록시 활성화 설정 @SpringBootApplication 안에 포함되어있음 (따로 설정 필요 X)
 public class WeatherServiceApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/example/weather/service/DiaryService.java
+++ src/main/java/com/example/weather/service/DiaryService.java
@@ -4,9 +4,10 @@ import com.example.weather.domain.Diary;
 import com.example.weather.dto.DiaryDto;
 import com.example.weather.openApi.WeatherApiService;
 import com.example.weather.repository.DiaryRepository;
-import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Isolation;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -19,6 +20,7 @@ public class DiaryService {
     private final DiaryRepository diaryRepository;
     private final WeatherApiService weatherApiService;
 
+    @Transactional(isolation = Isolation.SERIALIZABLE)
     public void createDiary(LocalDate date, String text) {
         Map<String, Object> weatherData = weatherApiService.getWeatherData();
 
@@ -31,24 +33,27 @@ public class DiaryService {
             .build());
     }
 
+    @Transactional(readOnly = true)
     public List<DiaryDto> readDiary(LocalDate date) {
         return diaryRepository.findAllByDate(date).stream()
             .map(DiaryDto::fromEntity)
             .collect(Collectors.toList());
     }
 
+    @Transactional(readOnly = true)
     public List<DiaryDto> readDiaries(LocalDate startDate, LocalDate endDate) {
         return diaryRepository.findAllByDateBetween(startDate, endDate).stream()
             .map(DiaryDto::fromEntity)
             .collect(Collectors.toList());
     }
 
+    @Transactional
     public void updateDiary(LocalDate date, String text) {
         Diary nowDiary = diaryRepository.getFirstByDate(date);
         nowDiary.updateText(text);
         diaryRepository.save(nowDiary);
     }
-    
+
     @Transactional
     public void deleteDiary(LocalDate date) {
         diaryRepository.deleteAllByDate(date);
